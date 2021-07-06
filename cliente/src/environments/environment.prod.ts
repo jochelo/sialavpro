d88583569ca1e118b26a248775df1c9d1472c49d
Jose Manuel Bilbao Quia <jochelo_mbq23@hotmail.com>
2021-07-06 05:06:15 -0400

    cors

--- cliente/src/environments/environment.prod.ts
+++ cliente/src/environments/environment.prod.ts
@@ -1,6 +1,8 @@
 export const environment = {
   production: true,
+  base: 'https://www.ke-malla.com/api/',
+  base3: 'http://15.228.49.255/api/',
   base1: 'http://localhost:8000/api/',
   base0: 'http://192.168.0.16:8000/api/',
-  base: 'http://192.168.1.12:8000/api/',
+  base2: 'http://192.168.1.12:8000/api/',
 };
