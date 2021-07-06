263066626229817779859103a7e5ae3b0b5b2a81
Jose Manuel Bilbao Quia <jochelo_mbq23@hotmail.com>
2021-07-06 01:31:35 -0400

    migration users

--- servidor/database/migrations/2014_10_12_000000_create_users_table.php
+++ servidor/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('nomre');
+            $table->string('nombre');
             $table->string('email')->unique();
             $table->string('cuenta')->unique();
             $table->string('password');
